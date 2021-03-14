00fa8c53ced01cfb087fb02b89567ab7d40a23f7
ChampionBuffalo <49675578+ChampionBuffalo1@users.noreply.github.com>
2021-03-14 18:42:19 +0530

    old App.js

--- App.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Main from './components/MainScreen';
-import { enableScreens } from 'react-native-screens';
-enableScreens();
-
-export default function App() {
-  return <Main />;
-}
